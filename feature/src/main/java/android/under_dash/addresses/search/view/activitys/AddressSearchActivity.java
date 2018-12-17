c01201bc9dc3ee8126cb3e46e2ef0039f9286668
Shimi Ulman <shimiuhlman@gmail.com>
2018-12-17 10:18:02 +0200

    pass Address to get addressInfo call

--- feature/src/main/java/android/under_dash/addresses/search/view/activitys/AddressSearchActivity.java
+++ feature/src/main/java/android/under_dash/addresses/search/view/activitys/AddressSearchActivity.java
@@ -201,8 +201,8 @@ public class AddressSearchActivity extends Activity_ {
     private void initAddressData() {
 
         App.getBackgroundHandler().post(() -> {
-            Box<AddressResultList> addressBox = getBox(AddressResultList.class);
-            List<AddressResultList> addresses = addressBox.getAll();
+            Box<AddressSearchList> addressBox = getBox(AddressSearchList.class);
+            List<AddressSearchList> addresses = addressBox.getAll();
             //StringBuilder destination = new StringBuilder();
             List<String> destination = new ArrayList<>();
             if(addresses != null && addresses.size() > 0 && addresses.get(0).getDuration() == 0) {
@@ -217,7 +217,7 @@ public class AddressSearchActivity extends Activity_ {
                 }
 
                 Log.d("shimi", "in create addresses.size() = "+addresses.size()+ "  destination = "+destination.toString());
-                HttpHelper.getDistanceInfoAndAddInDb(destination,destination);
+                HttpHelper.getDistanceInfoAndAddInDb(addresses, addresses);
                 //HttpHelper.getDistanceInfoAndAddInDb("New+York+City,NY",destination.toString(),null);
             }else{
                 if(addresses != null) {
@@ -252,10 +252,10 @@ public class AddressSearchActivity extends Activity_ {
     }
 
     private void updateListData() {
-        mAdapter.setData(new ArrayList<AddressResultList>());
+        mAdapter.setData(new ArrayList<AddressSearchList>());
         App.getUiHandler().postDelayed(new Runnable() {
             @Override public void run() {
-                Box<AddressResultList> addressBox = getBox(AddressResultList.class);
+                Box<AddressSearchList> addressBox = getBox(AddressSearchList.class);
                 mItemAnimation.getAnimation().reset();
                 mRecyclerView.setLayoutAnimation(mItemAnimation);
                 mAdapter.setData(addressBox.getAll());
