e9dbac3a54f6b6f0a0196c2c2fc58303548d3b77
shimiuh <shimiuhlman@gmail.com>
2019-01-14 17:32:55 +0200

    cleanup

--- feature/src/main/java/android/under_dash/addresses/search/app/Constants.java
+++ feature/src/main/java/android/under_dash/addresses/search/app/Constants.java
@@ -4,10 +4,7 @@ import android.under_dash.addresses.search.R;
 
 public class Constants {
 
-
-//    public static final String ADDRESS_RESULT = "ADDRESS_RESULT";
-//    public static final String ADDRESS_SEARCH = "ADDRESS_SEARCH";
-    public static final int DIALOG_EDIT_TEXT_ID = R.id.addresses_list;
-    public static final float COST_PER_ELEMENT = 00.5f;
+    public static final int    DIALOG_EDIT_TEXT_ID = R.id.addresses_list;
+    public static final float  COST_PER_ELEMENT    = 00.5f;
     public static final String GOOGLE_API_KEY_PATH = "/storage/emulated/0/Documents/GoogleApiKey.csv";
 }
