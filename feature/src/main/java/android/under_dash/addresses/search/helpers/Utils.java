28860b31d0163cf5db0f067ca3c23637cafa60b1
shimiuh <shimiuhlman@gmail.com>
2019-01-17 12:06:34 +0200

    added getElevation() method

--- feature/src/main/java/android/under_dash/addresses/search/helpers/Utils.java
+++ feature/src/main/java/android/under_dash/addresses/search/helpers/Utils.java
@@ -20,25 +20,65 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Utils {
 
-    private static final String TAG = "GeocodingLocation";
+    private static final String TAG = Utils.class.getSimpleName();
 
-    public static String getLatLongFromLocation(final String locationAddress) {
+    // Call must be of the Ui thread
+    public static Address getLatLongFromLocation(final String locationAddress) {
         Geocoder geocoder = new Geocoder(App.get(), Locale.getDefault());
-        String result = "";
+        Address result = new Address( Locale.getDefault());
         try {
             List<Address> addressList = geocoder.getFromLocationName(locationAddress, 5);
             if (addressList != null && addressList.size() > 0) {
-                Address address = addressList.get(0);
-                StringBuilder sb = new StringBuilder();
-                sb.append(address.getLatitude()).append(",").append(address.getLongitude());
-                result = sb.toString();
+                result = addressList.get(0);
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(address.getLatitude()).append(",").append(address.getLongitude());
+//                result = sb.toString();
             }
         } catch (Exception e) {
             Log.e("shimi", "Unable to connect to Geocoder", e);
         }
         return result;
+    }
+
+    // Call must be of the Ui thread
+    public static double getElevation(final String locationAddress) {
+        Address address = getLatLongFromLocation(locationAddress);
+        return getElevation(address.getLongitude(), address.getLatitude());
+    }
+    // Call must be of the Ui thread
+    public static double getElevation(double longitude, double latitude) {
+        final double[] result = {Double.NaN};
+        OkHttpClient httpClient = new OkHttpClient();
+        String url = "http://maps.googleapis.com/maps/api/elevation/"
+                + "xml?locations=" + String.valueOf(latitude)
+                + "," + String.valueOf(longitude)
+                + "&sensor=true";
+        Request request = new Request.Builder().url(url).build();
+        try {
+            Response response = httpClient.newCall(request).execute();
+            String respStr = response.toString();
+            String tagOpen = "<elevation>";
+            String tagClose = "</elevation>";
+            if (respStr.contains(tagOpen)) {
+                int start = respStr.indexOf(tagOpen) + tagOpen.length();
+                int end = respStr.indexOf(tagClose);
+                String value = respStr.substring(start, end);
+                result[0] = Double.parseDouble(value);//(double)(Double.parseDouble(value)*3.2808399); // convert from meters to feet
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+// code based of https://stackoverflow.com/questions/1995998/get-altitude-by-longitude-and-latitude-in-android
+
+        return result[0];
     }
 
     public static boolean isBadPosSize(List mList, int position) {
